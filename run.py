712529d2aac167cd4df629b0211c86ced28c9582
rcriii42 <rcriii42@gmail.com>
2023-03-16 11:50:31 -0700

    List default options in help

--- run.py
+++ run.py
@@ -9,10 +9,10 @@ from server import server
 parser = argparse.ArgumentParser(description='Show nav chart with various layers')
 parser.add_argument('--layers',
                     default="1,2,3,5",
-                    help='Comma delimited list of layers to show (no spaces)')
+                    help='Comma delimited list of layers (1 - 12) to show. Default is 1,2,3,5')
 parser.add_argument('--type',
                     default="paper",
-                    help='The map type (Paper, ENC)')
+                    help='The map type (paper, enc), default is paper')
 
 args = parser.parse_args()
 print(f'{args.type} {args.layers}')
