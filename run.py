e87fc5a2cee21fd7823863bdef4ef5dc16f6fcea
rcriii42 <rcriii42@gmail.com>
2023-03-16 11:37:44 -0700

    Add default input values

--- run.py
+++ run.py
@@ -8,10 +8,13 @@ from server import server
 
 parser = argparse.ArgumentParser(description='Show nav chart with various layers')
 parser.add_argument('--layers',
+                    default="1,2,3,5",
                     help='Comma delimited list of layers to show (no spaces)')
 parser.add_argument('--type',
+                    default="paper",
                     help='The map type (Paper, ENC, Street)')
 
 args = parser.parse_args()
+print(f'{args.type} {args.layers}')
 
 server(layers=args.layers, chart_type=str.lower(args.type)).launch()
